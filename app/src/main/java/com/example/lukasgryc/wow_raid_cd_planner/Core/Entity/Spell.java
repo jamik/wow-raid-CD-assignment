c9ce5bbd405e0d4b2793dc3bcec2197657c32b6a
Lukas Gryc <jamaicacz@gmail.com>
2017-09-13 16:59:34 +0200

    Init repo

---
+++ app/src/main/java/com/example/lukasgryc/wow_raid_cd_planner/Core/Entity/Spell.java
@@ -0,0 +1,85 @@
+package com.example.lukasgryc.wow_raid_cd_planner.Core.Entity;
+
+/**
+ * Created by lukas.gryc on 12.9.2017.
+ */
+
+public class Spell {
+
+    public enum SpellEffectType {
+        HEALING,
+        REDUCTION,
+        ABSORB,
+        MANA,
+        HP_BOOST
+    };
+
+    public enum SpellCooldown {
+        CD_2_MIN(120),
+        CD_3_MIN(180),
+        CD_5_MIN(300);
+
+        private int timeInSec;
+
+        SpellCooldown(int val){
+            timeInSec = val;
+        }
+
+        public int getTimeInSec(){
+            return timeInSec;
+        }
+    };
+
+    SpellEffectType effect1;
+    SpellEffectType effect2;
+    SpellEffectType effect3;
+    int             effect1Val;
+    int             effect2Val;
+    int             effect3Val;
+    SpellCooldown   spellCd;
+    int             spellLasting;
+
+    public Spell(SpellEffectType effect1, SpellEffectType effect2, SpellEffectType effect3,
+                 int effect1Val, int effect2Val, int effect3Val, SpellCooldown spellCd, int spellLasting) {
+        this.effect1 = effect1;
+        this.effect2 = effect2;
+        this.effect3 = effect3;
+        this.effect1Val = effect1Val;
+        this.effect2Val = effect2Val;
+        this.effect3Val = effect3Val;
+        this.spellCd = spellCd;
+        this.spellLasting = spellLasting;
+    }
+
+    public SpellEffectType getEffect1() {
+        return effect1;
+    }
+
+    public SpellEffectType getEffect2() {
+        return effect2;
+    }
+
+    public SpellEffectType getEffect3() {
+        return effect3;
+    }
+
+    public int getEffect1Val() {
+        return effect1Val;
+    }
+
+    public int getEffect2Val() {
+        return effect2Val;
+    }
+
+    public int getEffect3Val() {
+        return effect3Val;
+    }
+
+    public SpellCooldown getSpellCd() {
+        return spellCd;
+    }
+
+    public int getSpellLasting() {
+        return spellLasting;
+    }
+}
